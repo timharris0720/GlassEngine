dfeebeb63f90ecfdb92678092c1c00406085fd8d
tim <timharris0307@proton.me>
2025-05-07 23:12:31 +0100

    Build Script Auto Get Processor Count (windows)

--- buildProject_D.bat
+++ buildProject_D.bat
@@ -1,9 +1,9 @@
 @echo off
-cmake --build build --config Debug -j 24
+cmake --build build --config Debug -j %NUMBER_OF_PROCESSORS%
 cmake --install build --config Debug
-cmake --build buildPlugins --config Debug -j 24
+cmake --build buildPlugins --config Debug -j %NUMBER_OF_PROCESSORS%
 cmake --install buildPlugins --config Debug
-cmake --build  "Test_project/testProj" --config Debug -j 24
+cmake --build  "Test_project/testProj" --config Debug -j %NUMBER_OF_PROCESSORS%
 cmake --install  "Test_project/testProj" --config Debug
 
 set /p DUMMY=Hit ENTER to continue
--- buildProject_R.bat
+++ buildProject_R.bat
@@ -1,8 +1,8 @@
 @echo off
-cmake --build build --config Release -j 24
+cmake --build build --config Release -j %NUMBER_OF_PROCESSORS%
 cmake --install build --config Release
-cmake --build buildPlugins --config Release -j 24
+cmake --build buildPlugins --config Release -j %NUMBER_OF_PROCESSORS%
 cmake --install buildPlugins --config Release
-cmake --build "Test_project/testProj" --config Release -j 24
+cmake --build "Test_project/testProj" --config Release -j %NUMBER_OF_PROCESSORS%
 cmake --install "Test_project/testProj" --config Release
 set /p DUMMY=Hit ENTER to continue...
